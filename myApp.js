6fd95906e354ba0ff88e158cc92fadde55c29461
dreamapple <2451731631@qq.com>
2014-12-06 12:25:38 +0800

    update

---
+++ myApp.js
@@ -0,0 +1,31 @@
+var myModule = angular.module("myModule", ['ngAnimate','ui.router']);
+
+myModule.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('form',{
+			url:'/form',
+			templateUrl:'form.html',
+			controller:'myFormController'
+		})
+		.state('form.required',{
+			url:'/required',
+			templateUrl:'form-required.html'
+		})
+		.state('form.optional',{
+			url:'/optional',
+			templateUrl:'form-optional.html'
+		})
+		.state('form.confirm',{
+			url:'/confirm',
+			templateUrl:'form-confirm.html'
+		});
+
+		$urlRouterProvider.otherwise('/form/required');
+}]);
+
+myModule.controller('myFormController', ['$scope', function($scope){
+	$scope.formData = {};
+	$scope.submit = function() {
+		alert("Cool, you have registered!");
+	};
+}]);
